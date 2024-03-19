f88a4622f39ad4540090c141753c8f73e97ce8f0
00014118 <eo@HOME-PC>
2024-03-20 01:42:54 +0500

    Angular Project v2

--- WAD14118.API/WAD14118.API/obj/Debug/net6.0/WAD14118.API.AssemblyInfo.cs
+++ WAD14118.API/WAD14118.API/obj/Debug/net6.0/WAD14118.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAD14118.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7c1f6993c59b757bd1df0edf358e2c97a36a052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d16ab5420eb886f4a718fc30dda609b7827fdb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAD14118.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAD14118.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
